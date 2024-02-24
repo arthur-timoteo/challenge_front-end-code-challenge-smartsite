3a3a3025b3dd2af96473d7bf75ef20db014a2620
arthur-timoteo <arthur.timoteo@gmail.com>
2024-02-23 22:36:28 -0300

    Add form control

--- smartfit/src/app/components/form/form.component.ts
+++ smartfit/src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  formGroup!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      period: '',
+      showClosedUnits: false
+    })
+  }
+
+  onSubmit(): void {
+    console.log("Submit");
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
 }
