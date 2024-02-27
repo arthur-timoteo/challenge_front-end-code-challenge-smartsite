95831fa5d7de1baeafda4f210b4cae71b951fbcc
arthur-timoteo <arthur.timoteo@gmail.com>
2024-02-26 22:53:37 -0300

    Add filter to closed units

--- smartfit/src/app/components/form/form.component.ts
+++ smartfit/src/app/components/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { UnitsService } from '../../services/units.service';
+import { Location } from '../../types/location';
 
 @Component({
   selector: 'app-form',
@@ -9,22 +10,44 @@ import { UnitsService } from '../../services/units.service';
 })
 export class FormComponent {
   formGroup!: FormGroup;
+  unitsBase: Location[] = [];
+  units: Location[] = [];
+  firstSearch: boolean = true;
+  filteredUnits1: Location[] = [];
 
   constructor(private formBuilder: FormBuilder, private unitService: UnitsService) {}
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       period: '',
-      showClosedUnits: false
+      showClosedUnits: true
     })
   }
 
   onSubmit(): void {
-    this.unitService.getAllUnits()
-      .subscribe(data => console.log(data));
+    if(this.firstSearch) {
+      this.unitService.getAllUnits()
+        .subscribe(data => {
+          this.unitsBase = data.locations;
+          
+          this.units = this.filterUnits();
+        });
+
+      this.firstSearch = false;
+    } else {
+      this.units = this.filterUnits();
+    }
   }
 
   onClean(): void {
     this.formGroup.reset();
+  }
+
+  filterUnits(): Location[] {
+    if(this.formGroup.value.showClosedUnits){
+      return this.unitsBase;
+    } else {
+      return this.unitsBase.filter(unit => unit.opened === true);
+    }
   }
 }
