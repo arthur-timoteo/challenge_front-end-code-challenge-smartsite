56221bdef93fb7084548eab133b9d4beb6bbc34d
arthur-timoteo <arthur.timoteo@gmail.com>
2024-03-03 11:39:30 -0300

    Create periods filter to units

--- smartfit/src/app/components/form/form.component.ts
+++ smartfit/src/app/components/form/form.component.ts
@@ -3,6 +3,23 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { UnitsService } from '../../services/units.service';
 import { Location } from '../../types/location';
 
+const PERIODS_HOUR = {
+  morning: {
+    first: '06',
+    last: '12'
+  },
+  afternoon: {
+    first: '12',
+    last: '18'
+  },
+  night: {
+    first: '18',
+    last: '23'
+  }
+}
+
+type PERIODS_INDEXES = 'morning' | 'afternoon' | 'night';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -31,23 +48,67 @@ export class FormComponent {
           this.unitsBase = data.locations;
           
           this.units = this.filterUnits();
+          console.log(this.units);
         });
 
       this.firstSearch = false;
     } else {
       this.units = this.filterUnits();
+      console.log(this.units);
     }
+  }
+
+  filterUnits(): Location[] {
+    let { period } = this.formGroup.value;
+
+    if(this.formGroup.value.showClosedUnits){
+      return period == '' ? this.unitsBase : this.filterUnitsByHour(this.unitsBase, period);
+    } else {
+      return period == '' ? this.unitsBase.filter(unit => unit.opened === true) : this.filterUnitsByHour(this.unitsBase.filter(unit => unit.opened === true), period);
+    }
+  }
+
+  transformWeekday(weekday: number){
+    switch (weekday) {
+      case 0:
+        return 'Dom.';
+      case 6:
+        return 'Sáb.';
+      default:
+        return 'Seg. à Sex.';
+    }
+  }
+
+  filterUnitsByHour(filteredUnits: Location[], period: string): Location[] {
+    let openHour = parseInt(PERIODS_HOUR[period as PERIODS_INDEXES].first);
+    let closeHour = parseInt(PERIODS_HOUR[period as PERIODS_INDEXES].last);
+
+    return filteredUnits.filter((unit) => { 
+      if(!unit.schedules) return true;
+
+      let weekday_now = this.transformWeekday(new Date().getDay());
+
+      for(let i = 0; i < unit.schedules.length; i++){
+        let schedule_hour = unit.schedules[i].hour;
+        let schedule_weekday = unit.schedules[i].weekdays;
+
+        if(weekday_now === schedule_weekday){
+          if(schedule_hour !== 'Fechada'){
+            let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ');
+            let unit_open_hour_int = parseInt(unit_open_hour.replace('h', ''), 10);
+            let unit_close_hour_int = parseInt(unit_close_hour.replace('h', ''), 10);
+  
+            if(unit_open_hour_int <= openHour && unit_close_hour_int >= closeHour) return true
+            else return false
+          }
+        }
+      }
+
+      return false;
+     });
   }
 
   onClean(): void {
     this.formGroup.reset();
-  }
-
-  filterUnits(): Location[] {
-    if(this.formGroup.value.showClosedUnits){
-      return this.unitsBase;
-    } else {
-      return this.unitsBase.filter(unit => unit.opened === true);
-    }
   }
 }
